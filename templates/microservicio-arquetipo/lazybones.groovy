f268fd3fd30b8cb4861b0d4389fcb7d8478258ed
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2019-01-24 11:40:38 -0500

    arquetipo v1.0

--- templates/microservicio-arquetipo/lazybones.groovy
+++ templates/microservicio-arquetipo/lazybones.groovy
@@ -1,8 +1,6 @@
 import uk.co.cacoethes.util.NameType
 import org.apache.commons.io.FileUtils
 
-
-
 Map props = [:]
 if (projectDir.name =~ /\-/) {
     props.project_class_name = transformText(projectDir.name, from: NameType.HYPHENATED, to: NameType.CAMEL_CASE)
@@ -15,8 +13,10 @@ props.project_group = ask("Define value for 'group' [org.example]: ", "org.examp
 props.project_name = ask("Define value for 'artifactId' [" + props.project_name + "]: ", props.project_name , "artifactId")
 props.project_version = ask("Define value for 'version' [0.1.0-SNAPSHOT]: ", "0.1.0-SNAPSHOT", "version")
 props.project_package = ask("Define value for 'package' [" + props.project_group + "]: ", props.project_group, "package")
-
-props.microservicio_arquetipo_version = '0.1.0'
+props.project_class_name = ask("Define value for 'className' [" + props.project_class_name + "]: ", props.project_class_name, "className").capitalize()
+props.project_property_name = transformText(props.project_class_name, from: NameType.CAMEL_CASE, to: NameType.PROPERTY)
+props.project_capitalized_name = props.project_property_name.capitalize()
+props.microservicio_arquetipo = '1.0.0'
 String packagePath = props.project_package.replace('.' as char, '/' as char)
 
 processTemplates 'pom.xml', props
@@ -30,12 +30,13 @@ def renameFile = { File from, String path ->
 }
 
 
-['src'].each { it ->
-    processTemplates it + '/pom.xml', props
-    processTemplates it + '/src/main/java/**/*.java', props
+['.'].each { it ->
 
-    File mainSources = new File(projectDir, it + '/src/main/java')
-    File mainResources = new File(projectDir, it + '/src/main/resources')
+
+    processTemplates '/src/main/java/**/*.java', props
+
+    File mainSources = new File(projectDir, '/src/main/java')
+    File mainResources = new File(projectDir,  '/src/main/resources')
 
     File mainSourcesPath = new File(mainSources, packagePath)
     File mainResourcesPath = new File(mainResources, packagePath)
@@ -53,4 +54,3 @@ def renameFile = { File from, String path ->
         renameFile(file, mainResourcesPath.absolutePath + '/' + file.name)
     }
 }
-
